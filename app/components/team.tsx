9b6b107780ef4e84c2d74bbe264c2038472f172d
Meet2054 <meetaparekh2003@gmail.com>
2024-09-30 02:45:46 +0530

    team page update

---
+++ app/components/team.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import img from '@/assets/parul.jpg'
+
+type Props = {}
+
+const Team = (props: Props) => {
+  return (
+    <div className='w-full h-auto min-h-[80vh] flex flex-col items-center justify-around text-white py-8'>
+      <div className='mb-8 p-2 text-center'>
+        <h1 className='text-5xl font-bold'>Head of Ayurveda Department</h1>
+      </div>
+      
+      <div className='flex flex-wrap justify-around gap-8 w-full px-16'>
+        {/* Team Member */}
+        {Array(4).fill('').map((_, idx) => (
+          <div key={idx} className='flex flex-col items-center gap-4'>
+            <div 
+              className='w-56 h-56 rounded-full bg-cover bg-center' 
+              style={{ backgroundImage: `url(${img.src})` }} 
+            />
+            <h1 className='font-bold text-2xl'>Meet Parekh</h1>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Team
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,15 +4,17 @@ import image from './image.png';
 import { CardDemo} from "../../components/eventList"
 import Hero from '../components/hero';
 import About from '../components/about';
+import Team from '../components/team';
 type Props = {}
 
 const Page = (props: Props) => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-700">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-200">
       {/* <InfiniteMovingCardsDemo/> */}
       <Hero/>
       <CardDemo/>
       <About/>
+      <Team/>
     </div>
   );
 }
