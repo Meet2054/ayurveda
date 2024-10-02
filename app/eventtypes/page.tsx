0ae04e5ea1333cc85c80b35af64ad1535b83e3af
Meet2054 <meetaparekh2003@gmail.com>
2024-10-02 21:50:01 +0530

    .

--- app/eventtypes/page.tsx
+++ app/eventtypes/page.tsx
@@ -6,9 +6,10 @@ type Props = {}
 const Page = (props: Props) => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
+      
       <CardDemo category="Blockchain" />  {/* Render cards from 'Blockchain' category */}
       <CardDemo category="Virtual" />     {/* Render cards from 'Virtual' category */}
-      <CardDemo category="Upcoming" />    {/* Render cards from 'Upcoming' category */}
+      {/* <CardDemo category="Upcoming" />    Render cards from 'Upcoming' category */}
     </div>
   );
 }
